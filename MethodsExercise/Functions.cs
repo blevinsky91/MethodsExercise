cdf9b85fe799553a5f44897f2b49aacf1b60c735
Brandon Levinsky <blevinsky91@gmail.com>
2023-06-19 15:15:22 -0400

    fixed errors on exercises

--- MethodsExercise/Functions.cs
+++ MethodsExercise/Functions.cs
@@ -3,28 +3,28 @@ namespace MethodsExercise
 {
 	public class Functions
 	{
-		static int Add(int num1, int num2)
+		public static int Add(int num1, int num2)
 		{
 			return num1 + num2;
-			Console.WriteLine(Add(7, 7));
+			
 		}
 
-		static int Sub(int num1, int num2)
+		public static int Sub(int num1, int num2)
 		{
 			return num1 - num2;
-			Console.WriteLine(Sub(7, 7));
+			
 		}
 
-		static int Mult(int num1, int num2)
+		public static int Mult(int num1, int num2)
 		{
 			return num1 * num2;
-			Console.WriteLine(Mult(7, 7));
+			
 		}
 
-		static int Div(int num1, int num2)
+		public static int Div(int num1, int num2)
 		{
 			return num1 / num2;
-			Console.WriteLine(Div(7, 7));
+			
 		}
 		
 
--- MethodsExercise/Program.cs
+++ MethodsExercise/Program.cs
@@ -3,18 +3,19 @@
     public class Program
     {
         static void Main(string[] args)
-        {   
+        {
+            Console.WriteLine(Functions.Add(7, 7));
+            Console.WriteLine(Functions.Sub(7, 7));
+            Console.WriteLine(Functions.Mult(7, 7));
+            Console.WriteLine(Functions.Div(7, 7));
             Console.WriteLine("What is your name?");
-            string name = Console.ReadLine();
-         
+            string? name = Console.ReadLine();
             Console.WriteLine("What is your favorite color?");
-
-            string color = Console.ReadLine();
-            
+            string? color = Console.ReadLine();
             Console.WriteLine("What is your favorite Animal?");
-            string animal = Console.ReadLine();
+            string? animal = Console.ReadLine();
             Console.WriteLine("What is your favorite band?");
-            string band = Console.ReadLine();
+            string? band = Console.ReadLine();
 
             Console.WriteLine($"Hello, my name is {name} and the coolest color is {color}. A {animal} is my favorite animal, and {band} makes the best music on the planet!");
         }
