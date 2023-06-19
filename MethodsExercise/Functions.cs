49f226c5c1838bdf6f8e51a6babeaf08fed6c3f1
Brandon Levinsky <blevinsky91@gmail.com>
2023-06-19 12:24:45 -0400

    both exercises complete

--- MethodsExercise/Functions.cs
+++ MethodsExercise/Functions.cs
@@ -3,9 +3,31 @@ namespace MethodsExercise
 {
 	public class Functions
 	{
-		public Functions()
+		static int Add(int num1, int num2)
 		{
+			return num1 + num2;
+			Console.WriteLine(Add(7, 7));
 		}
+
+		static int Sub(int num1, int num2)
+		{
+			return num1 - num2;
+			Console.WriteLine(Sub(7, 7));
+		}
+
+		static int Mult(int num1, int num2)
+		{
+			return num1 * num2;
+			Console.WriteLine(Mult(7, 7));
+		}
+
+		static int Div(int num1, int num2)
+		{
+			return num1 / num2;
+			Console.WriteLine(Div(7, 7));
+		}
+		
+
 	}
 }
 
